8da3fbc751373cd3c03f16648508c49caa7685ff
signeus <coolasduck09@gmail.com>
2016-11-03 20:19:25 +0100

    initial commit

---
+++ app/src/main/java/com/apps/signeus/agecalculator/LaunchScreen.java
@@ -0,0 +1,13 @@
+package com.apps.signeus.agecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_screen);
+    }
+}
